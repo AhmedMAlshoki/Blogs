79033f6138bb30d90f9e26be4100f2ed77d5f888
AhmedMAlshoki <ahmed.m.alshoki@gmail.com>
2025-08-09 15:10:32 +0300

    Initial structure for Security system , Security Config & Security Services

--- src/main/java/com/example/Blogs/Config/SecurityConfiguration.java
+++ src/main/java/com/example/Blogs/Config/SecurityConfiguration.java
@@ -1,9 +1,26 @@
 package com.example.Blogs.Config;
 
+import com.example.Blogs.Services.Security.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    @Autowired
+    private UserDetailsServiceImpl  userDetailsService;
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
 }
